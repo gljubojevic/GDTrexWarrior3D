9c50f1c0b6c33e7bce407d8abc3534f7c3a0d17e
gljubojevic <gljubojevic@users.noreply.github.com>
2026-01-31 23:53:19 +0100

    Make enemy follow player
    - has test enemy in arena

--- enemies/drone.gd
+++ enemies/drone.gd
@@ -1,7 +1,10 @@
 extends RigidBody3D
 
 @onready var drone_fighter: Node3D = %drone_fighter
-@onready var energy = 100.0
+@onready var energy: float = 100.0
+@onready var player: CharacterBody3D = get_node("/root/arena01/Player")
+
+var speed = randf_range(5.0, 10.0)
 
 # Called when the node enters the scene tree for the first time.
 #func _ready() -> void:
@@ -10,6 +13,12 @@ extends RigidBody3D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #	pass
+
+func _physics_process(delta: float) -> void:
+	var direction = global_position.direction_to(player.global_position)
+	direction.y = 0.0
+	linear_velocity = direction * speed
+	drone_fighter.rotation.y = Vector3.FORWARD.signed_angle_to(direction, Vector3.UP) + PI
 
 # reduce energy when hit or die
 func take_damage(value: float):
