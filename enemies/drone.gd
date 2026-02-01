f004f17daa83dc94839863c72f6d8c5abe0cfb67
gljubojevic <gljubojevic@users.noreply.github.com>
2026-02-01 09:25:02 +0100

    Add remove enemy after death
    - add physics anim for death

--- enemies/drone.gd
+++ enemies/drone.gd
@@ -1,9 +1,10 @@
 extends RigidBody3D
 
 @onready var drone_fighter: Node3D = %drone_fighter
-@onready var energy: float = 100.0
 @onready var player: CharacterBody3D = get_node("/root/arena01/Player")
+@onready var timer: Timer = %Timer
 
+var energy: float = 100.0
 var speed = randf_range(5.0, 10.0)
 
 # Called when the node enters the scene tree for the first time.
@@ -22,8 +23,23 @@ func _physics_process(delta: float) -> void:
 
 # reduce energy when hit or die
 func take_damage(value: float):
+	# no more energy, no more damage
+	if energy == 0:
+		return
+	# do damage
 	energy -= value
-	if energy < 0:
-		queue_free()		# die when no more energy
-	else:
-		drone_fighter.demage()	# start animation for 
+	if energy > 0:
+		drone_fighter.demage()	# start animation for
+		return
+	# start die process with some animation
+	energy = 0
+	set_physics_process(false)
+	var direction = player.global_position.direction_to(global_position)
+	var random_upward_force = Vector3.UP * randf_range(5.0, 15.0)
+	apply_central_impulse(direction.rotated(Vector3.UP, randf_range(-0.2, 0.2)) * 20.0 + random_upward_force)
+	# start timer to remove
+	timer.start()
+
+# remove from scene
+func _on_timer_timeout() -> void:
+	queue_free()	
